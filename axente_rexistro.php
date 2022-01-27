f35538e079fb81e3b0ad83df12b512dc50715ac1
PazitoPazos <javier.pazos001@gmail.com>
2022-01-27 11:52:00 +0100

    Commit final

--- axente_rexistro.php
+++ axente_rexistro.php
@@ -1,6 +1,6 @@
 <?php
     header('Content-Type: text/html; charset=UTF-8');
-    echo "<pre>"; print_r($_REQUEST); echo "</pre>";
+    echo "<pre>"; print_r($_REQUEST); echo "</pre>"; 
 
     $nomeCompleto=htmlspecialchars(trim(strip_tags($_REQUEST['nomeCompleto'])),ENT_QUOTES, "ISO-8859-1");
     if ($nomeCompleto == "")
